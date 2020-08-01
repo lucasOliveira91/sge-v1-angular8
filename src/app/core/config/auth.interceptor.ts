a6ce00a8a982a0f45905a52900d892ecb803c671
Lucas Oliveira <ldoliveira91@outlook.com>
2020-08-01 10:42:24 -0300

    adicionando credentials

--- src/app/core/config/auth.interceptor.ts
+++ src/app/core/config/auth.interceptor.ts
@@ -12,9 +12,9 @@ export class AuthInterceptor implements HttpInterceptor {
     ) { }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // request = request.clone({
-        //     withCredentials: true
-        //   });
+        request = request.clone({
+            withCredentials: true
+          });
         return next.handle(request);
     }
 }
